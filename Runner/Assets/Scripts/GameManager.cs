05867aa0db8a74b438b5741600d6cb4c86165638
KimColl <73173379+KimColl@users.noreply.github.com>
2021-12-28 16:06:54 +0100

    28/12/2021
    Created the Welcome Scene with a  title and Button Play
    
    AND
    
    Created the UML diagram for the coin animation

---
+++ Runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //is a library unity which allows to manage the scenes
+                                   //it is used so that I can load the scenes one after the other
+
+public class GameManager : MonoBehaviour
+{
+    //it will load the first scene of the project
+    public void StartScene()
+    {
+        //SceneManager will be used so it can read the scenes which are in unity
+        SceneManager.LoadScene(0);
+        //LoadScene is a method
+    }
+
+    //loads the scene with the name of GameScene
+    public void Game()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    //loads the scene with the name of GameScene2
+    public void GameLevel2()
+    {
+        SceneManager.LoadScene("GameScene2");
+    }
+
+    //loads the scene with the name of EndScene
+    public void EndGame()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
